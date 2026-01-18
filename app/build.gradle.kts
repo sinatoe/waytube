f06aa1414e416030632e4c195973941af4c91ddc
sinatoe <219674570+sinatoe@users.noreply.github.com>
2026-01-18 09:50:45 +0100

    chore: bump version to 0.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ configure<ApplicationExtension> {
         applicationId = "com.waytube.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
