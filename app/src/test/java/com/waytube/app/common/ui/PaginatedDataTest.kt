b3bf6b5486233c1f15666886e3b3f3731f8b4456
sinatoe <219674570+sinatoe@users.noreply.github.com>
2026-04-21 09:13:47 +0200

    test: add a unit test for paginated data flow transitions

---
+++ app/src/test/java/com/waytube/app/common/ui/PaginatedDataTest.kt
@@ -0,0 +1,74 @@
+package com.waytube.app.common.ui
+
+import app.cash.turbine.test
+import com.waytube.app.common.domain.Page
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class PaginatedDataTest {
+    @Test
+    fun `test sequential paginated fetch responses`() = runTest {
+        val nextResultIterator = iterator {
+            yield(Result.failure(Exception()))
+            yield(Result.success(Page(items = listOf(Unit), next = null)))
+        }
+
+        val resultIterator = iterator {
+            yield(Result.failure(Exception()))
+            yield(
+                Result.success(
+                    Page(
+                        items = listOf(Unit),
+                        next = nextResultIterator::next
+                    )
+                )
+            )
+        }
+
+        val flow = PaginatedData.createFlow(resultIterator::next)
+
+        flow.test {
+            awaitItem().let { data ->
+                assertTrue(data.items.isEmpty())
+                assertTrue(
+                    (data.state as? PaginatedData.State.Idle)
+                        ?.also { it.load() } != null
+                )
+            }
+
+            assertTrue(awaitItem().state is PaginatedData.State.Loading)
+
+            assertTrue(
+                (awaitItem().state as? PaginatedData.State.Error)
+                    ?.also { it.retry() } != null
+            )
+
+            assertTrue(awaitItem().state is PaginatedData.State.Loading)
+
+            awaitItem().let { data ->
+                assertTrue(data.items.size == 1)
+                assertTrue(
+                    (data.state as? PaginatedData.State.Idle)
+                        ?.also { it.load() } != null
+                )
+            }
+
+            assertTrue(awaitItem().state is PaginatedData.State.Loading)
+
+            assertTrue(
+                (awaitItem().state as? PaginatedData.State.Error)
+                    ?.also { it.retry() } != null
+            )
+
+            assertTrue(awaitItem().state is PaginatedData.State.Loading)
+
+            awaitItem().let { data ->
+                assertTrue(data.items.size == 2)
+                assertTrue(data.state is PaginatedData.State.Done)
+            }
+
+            expectNoEvents()
+        }
+    }
+}
