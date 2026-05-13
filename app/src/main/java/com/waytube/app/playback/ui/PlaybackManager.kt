c1f250196dc07fe7181392e9cb353ffce865c1a8
sinatoe <219674570+sinatoe@users.noreply.github.com>
2026-05-13 20:26:45 +0200

    feat(playback): disable video track when app goes to background

--- app/src/main/java/com/waytube/app/playback/ui/PlaybackManager.kt
+++ app/src/main/java/com/waytube/app/playback/ui/PlaybackManager.kt
@@ -1,44 +1,76 @@
 package com.waytube.app.playback.ui
 
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ProcessLifecycleOwner
+import androidx.media3.common.C
 import androidx.media3.common.Player
 import androidx.media3.session.MediaController
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.awaitCancellation
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.transformLatest
 import kotlinx.coroutines.guava.await
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class PlaybackManager(private val controllerBuilder: MediaController.Builder) {
     private val activeId = MutableStateFlow<String?>(null)
 
+    private val isAppInBackgroundFlow = callbackFlow {
+        val lifecycle = ProcessLifecycleOwner.get().lifecycle
+
+        val observer = object : DefaultLifecycleObserver {
+            override fun onStart(owner: LifecycleOwner) {
+                trySend(false)
+            }
+
+            override fun onStop(owner: LifecycleOwner) {
+                trySend(true)
+            }
+        }
+
+        trySend(!lifecycle.currentState.isAtLeast(Lifecycle.State.STARTED))
+
+        lifecycle.addObserver(observer)
+
+        awaitClose {
+            lifecycle.removeObserver(observer)
+        }
+    }
+
+    private val playerFlow = callbackFlow {
+        val future = controllerBuilder.buildAsync()
+        val controller = future.await()
+
+        trySend(controller)
+
+        awaitClose {
+            controller.apply {
+                stop()
+                clearMediaItems()
+            }
+            MediaController.releaseFuture(future)
+        }
+    }
+        .transformLatest { player ->
+            emit(player)
+
+            isAppInBackgroundFlow.collect { isBackground ->
+                player.trackSelectionParameters = player.trackSelectionParameters
+                    .buildUpon()
+                    .setTrackTypeDisabled(C.TRACK_TYPE_VIDEO, isBackground)
+                    .build()
+            }
+        }
+
     fun requestPlayer(id: String): Flow<Player?> =
         activeId.flatMapLatest { activeId ->
-            if (activeId == id) {
-                flow {
-                    val future = controllerBuilder.buildAsync()
-                    try {
-                        val controller = future.await()
-
-                        try {
-                            emit(controller)
-                            awaitCancellation()
-                        } finally {
-                            controller.apply {
-                                stop()
-                                clearMediaItems()
-                            }
-                        }
-                    } finally {
-                        MediaController.releaseFuture(future)
-                    }
-                }
-            } else {
-                flowOf(null)
-            }
+            if (activeId == id) playerFlow else flowOf(null)
         }
 
     fun setActiveId(id: String?) {
